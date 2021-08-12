5c8b2a2aef3177dd7ebb136aa51c2ee16a17d44f
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-12 21:09:36 +0530

    [Daxil] Added:GetMapping with RequestParam

--- src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessageController.java
+++ src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessageController.java
@@ -13,10 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hellomessaging")
 
 public class HelloMessageController {
-    
+
     //	Annotations for mapping web requests on methods in request-handling classes with flexible method signatures.
     @GetMapping("/get")
     public String hello() {
         return "Hello from Bridgelabz";
+    }
+
+    // Using @RequestParam to pass name as query parameter
+    @GetMapping("/queryParam")
+    public String hello(@RequestParam(value = "fname") String fname, @RequestParam(value = "lname") String lname) {
+        return "Hello " + fname + " " + lname;
     }
 }
